6699335c1c06c9a4fe906d92f313d4a67b1eae43
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    sdm660-common: Switch to source built vendor.qti.hardware.perf@2.0.so
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57

--- sdm660.mk
+++ sdm660.mk
@@ -322,7 +322,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power-service \
-    android.hardware.power-impl
+    android.hardware.power-impl \
+    vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/config/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
