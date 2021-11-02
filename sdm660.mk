0ecef89ab723be5ed2ded3c2e424bbbd37896beb
kaderbava <ksbava7325@gmail.com>
2021-11-02 21:40:45 +0530

    sdm660-common: Add Pixel 2016 exclusive xml
    * This allows us to use unlimited backup in Google Photos infinitely.

--- sdm660.mk
+++ sdm660.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
