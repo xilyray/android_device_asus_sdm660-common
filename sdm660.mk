1b46d036e8c9aa1bd129a9f8bb5be34aca021270
a e s x i l y <aesxilymail@gmail.com>
2021-11-16 13:21:20 +0000

    sdm660-common: Adapt Project-Awaken [2/2]

--- sdm660.mk
+++ sdm660.mk
@@ -287,7 +287,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-awaken
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
